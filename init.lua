1a97ad738d42e547a02b3ac6d31512fb143b1c31
Toumazou <yiannistoumazou@gmail.com>
2025-10-11 13:23:46 +0100

    Restructured init.lua so I can follow what's going on

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
+-------------------------------------------------
 -- Bootstrap lazy.nvim
+-------------------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -13,12 +15,75 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
+
+-- Put lazy into the runtime path for neavim
 vim.opt.rtp:prepend(lazypath)
 
+
+-------------------------------------------------
+-- Set up settings before loading lazy.nvim so that mappings are correct
+-------------------------------------------------
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+--I think it enables more colour support of the terminal
+vim.opt.termguicolors = true 
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+
+-------------------------------------------------
+-- Set up bindings before loading lazy.nvim so that mappings are correct
+-------------------------------------------------
+vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+vim.keymap.set("i","jj","<Esc>", {noremap = true})
 
--- pull module with vim options, e.g., set tab width, global leader, etc.
-require("vim-options")
+-- J joins the current and next line, and below key map keeps your cursor in place
+vim.keymap.set("n", "J", "mzJ`z")
 
+
+
+-------------------------------------------------
 -- Setup lazy.nvim by giving plugins
+-------------------------------------------------
 require("lazy").setup("plugins")
+
+
+-------------------------------------------------
+-- Work in progress
+-------------------------------------------------
+
+--[[ 
+Not sure yet what these options do but could be usefull with undo trees (from primeagens file)
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenf("HOME") .. "/.vim.undodir"
+vim.opt.undofile = true
+]]
+
+--[[ 
+Purpose is to move multiple lines up and down but is buggy for some reason
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<>gv=gv")
+]]
